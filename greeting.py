5eb5947e46399ed33c12022461624600ebf41150
squigglezworth <squigglezworth@gmail.com>
2019-04-14 00:21:40 -0400

    Add basic logging

--- greeting.py
+++ greeting.py
@@ -1,11 +1,10 @@
 from sopel.config.types import StaticSection, ChoiceAttribute, ValidatedAttribute
 from sopel.module import rule, event, interval
 from sopel.tools import SopelMemory
+from sopel.logger import get_logger
 import time
 
-
-timeout = greeting = None
-
+timeout = greeting = logger = None
 
 class GreetingSection(StaticSection):
     timeout  = ValidatedAttribute('timeout', int)
@@ -20,12 +19,13 @@ def configure(config):
 
 
 def setup(bot):
-    global timeout, greeting
+    global timeout, greeting, logger
 
     bot.config.define_section('greeting', GreetingSection)
 
     timeout = bot.config.greeting.timeout
     greeting = bot.config.greeting.greeting
+    logger = get_logger(__name__)
 
     if 'greeting' not in bot.memory:
         bot.memory['greeting'] = SopelMemory()
@@ -34,11 +34,17 @@ def setup(bot):
 @event('JOIN')
 @rule('.*')
 def joined(bot, trigger):
+    logger.info(trigger.nick + ' joined')
+
     if bot.nick == trigger.nick:
+        logger.info('Skipping self')
+
         return
     else:
         uid = bot.db.get_nick_id(trigger.nick, create=True)
         jtime = time.time()
+
+        logger.info('Starting listening for first message from ' + trigger.nick)
 
         bot.memory['greeting'][uid] = jtime
 
@@ -46,14 +52,22 @@ def joined(bot, trigger):
 @rule('.*')
 def speak(bot, trigger):
     global timeout, greeting
-    uid = bot.db.get_nick_id(trigger.nick, create=True)
     ctime = time.time()
+    uid = bot.db.get_nick_id(trigger.nick, create=True)
+
+    logger.info('Checking message from ' + trigger.nick)
 
     if uid in bot.memory['greeting']:
+        logger.info('Entry found, checking ' + trigger.nick)
+
         jtime = bot.memory['greeting'][uid]
 
         if ctime - jtime <= timeout:
+            logger.info('Greeting ' + trigger.nick)
+
             bot.say(greeting)
+
+        logger.info('Removing entry for ' + trigger.nick)
 
         del bot.memory['greeting'][uid]
 
@@ -62,9 +76,13 @@ def speak(bot, trigger):
 @event('QUIT')
 @rule('.*')
 def cleanup_events(bot, trigger):
+    logger.info('Cleaning up from PART/QUIT')
+
     uid = bot.db.get_nick_id(trigger.nick, create=True)
 
     if uid in bot.memory['greeting']:
+        logger.info('Removing entry for ' + trigger.nick)
+
         del bot.memory['greeting'][uid]
 
 
@@ -73,6 +91,10 @@ def cleanup_interval(bot):
     global timeout
     ctime = time.time()
 
+    logger.info('Cleanup time')
+
     for key, val in bot.memory['greeting'].items():
         if ctime - val > timeout:
+            logger.info('Removing entry for ' + key)
+
             del bot.memory['greeting'][key]
