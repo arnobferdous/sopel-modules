9344e7fdc51bb84b2999af1a98c01785e039847c
squigglezworth <squigglezworth@gmail.com>
2019-04-14 21:01:01 -0400

    faq.py - Very simple FAQ module

---
+++ faq.py
@@ -0,0 +1,12 @@
+from sopel.module import commands
+
+answers = {
+	"": ""
+}
+
+@commands("faq")
+def faq(bot, trigger):
+    question = trigger.match.group(2)
+
+    if question in answers:
+    	bot.say(answers[question])
