50fa27743cc44e83933499e30267bdbc22ec2882
squigglezworth <squigglezworth@gmail.com>
2019-04-15 06:33:29 -0400

    faq.py - Read questions/answers from user-configured file

--- faq.py
+++ faq.py
@@ -1,11 +1,26 @@
 from sopel.module import commands
+from sopel.config.types import StaticSection, ValidatedAttribute
 import json, os
+
+
+class FaqSection(StaticSection):
+    path  = ValidatedAttribute('path')
+
+
+def configure(config):
+    config.define_section('faq', FaqSection, validate=False)
+
+    config.faq.configure_setting('path', 'Path where questions/answers are stored')
 
 
 def setup(bot):
     global answers
 
-    answers = json.load(open(os.path.dirname(os.path.realpath(__file__)) + '/data/faq'))
+    bot.config.define_section('faq', FaqSection)
+
+    path = bot.config.faq.path
+
+    answers = json.load(open(path))
 
 
 @commands("faq")
