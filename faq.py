0866473cb4eaff46de4eddff9e071f181a02bd0c
squigglezworth <squigglezworth@gmail.com>
2019-04-14 22:49:05 -0400

    faq.py - Move questions/answers into file

--- faq.py
+++ faq.py
@@ -1,8 +1,12 @@
 from sopel.module import commands
+import json, os
 
-answers = {
-"": ""
-}
+
+def setup(bot):
+    global answers
+
+    answers = json.load(open(os.path.dirname(os.path.realpath(__file__)) + '/data/faq'))
+
 
 @commands("faq")
 def faq(bot, trigger):
